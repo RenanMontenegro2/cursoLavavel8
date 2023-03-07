fad6ee3be561e1e823a63af8422c16227b8f392c
RenanMontenegro2 <126257350+RenanMontenegro2@users.noreply.github.com>
2023-03-07 09:32:41 -0300

    First Commit

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\ProdutoControllers;
+
+class ProdutoController extends Controller
+{
+    //public function index
+    public function index(){                            
+        return "index";
+    }
+}
